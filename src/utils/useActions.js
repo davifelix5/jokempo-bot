bf30208af69c754737367d5dabcdc127dac00a52
Davi F <davi.felix.lima@gmail.com>
2021-12-28 15:31:43 -0300

    fix: format code better

--- src/utils/useActions.js
+++ src/utils/useActions.js
@@ -6,28 +6,35 @@ module.exports = {
     useActions(actions, actionsMessages) {
       return {
         index(message, args) {
+
           try {
             const [action, ...secondArgs] = args
+
             if (!action) {
-                message.channel.send({
+                return message.channel.send({
                     embed: actionsMessages.tutorial(),
                 })
                 .then(msg => {
                     msg.react('👍')
                 })
-                return
             }
+
             const actionList = Object.keys(actions).filter(action => !action.includes('Validate'))
             if (!actionList.includes(action))
                 throw new ArgumentError(`Função "${action}" desconhecida`)
+
             const validationFunction = actions[`${action}Validate`]
             const validatedArgs = validationFunction && validationFunction(message, secondArgs)
+
             actions[action](message, validatedArgs)
+
         } catch (err) {
+
             if (err instanceof ArgumentError)
                 message.channel.send({embed: messages.announceError(err.message)})
             else
                 console.log('Houve um erro: ' + err)
+
         }
       }
     }
